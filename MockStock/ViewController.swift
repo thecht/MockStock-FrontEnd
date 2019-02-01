36ca60c7d29aee53a6ff41e860fe8af0fda2623f
Trey <thecht182@gmail.com>
2019-02-01 08:49:02 -0500

    Initial Commit

---
+++ MockStock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockStock
+//
+//  Created by Theodore Hecht on 2/1/19.
+//  Copyright © 2019 Theodore Hecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
