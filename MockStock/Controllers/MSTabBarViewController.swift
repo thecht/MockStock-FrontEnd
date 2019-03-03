d35f47a610499a799ed859901159c719235a5700
Trey <thecht182@gmail.com>
2019-03-02 20:26:01 -0500

    adds bar buttons.

--- MockStock/Controllers/MSTabBarViewController.swift
+++ MockStock/Controllers/MSTabBarViewController.swift
@@ -18,17 +18,41 @@ class MSTabBarViewController: UIViewController {
         v.backgroundColor = UIColor.white
         return v
     }()
+    var separationBar: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.backgroundColor = UIColor.gray
+        return v
+    }()
     var pageIndicator: UIView = {
         return UIView()
     }()
     var portfolioButton: UIButton = {
-        return UIButton()
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("PORTFOLIO", for: .normal)
+        b.titleLabel?.font = UIFont(name: "Futura-CondensedExtraBold", size: 18)
+        b.setTitleColor(.gray, for: .normal)
+        b.setTitleColor(.black, for: .selected)
+        return b
     }()
     var marketplaceButton: UIButton = {
-        return UIButton()
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("MARKETPLACE", for: .normal)
+        b.titleLabel?.font = UIFont(name: "Futura-CondensedExtraBold", size: 18)
+        b.setTitleColor(.gray, for: .normal)
+        b.setTitleColor(.black, for: .selected)
+        return b
     }()
     var leaguesButton: UIButton = {
-        return UIButton()
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("LEAGUE", for: .normal)
+        b.titleLabel?.font = UIFont(name: "Futura-CondensedExtraBold", size: 18)
+        b.setTitleColor(.gray, for: .normal)
+        b.setTitleColor(.black, for: .selected)
+        return b
     }()
     
     // ViewController Pages
@@ -40,6 +64,7 @@ class MSTabBarViewController: UIViewController {
     override func viewDidLoad() {
         view.backgroundColor = UIColor.clear
         view.addSubview(contentView)
+        contentView.addSubview(separationBar)
         contentView.addSubview(portfolioButton)
         contentView.addSubview(marketplaceButton)
         contentView.addSubview(leaguesButton)
@@ -51,6 +76,50 @@ class MSTabBarViewController: UIViewController {
         contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -75).isActive = true
         
+        separationBar.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        separationBar.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        separationBar.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        separationBar.heightAnchor.constraint(equalToConstant: 1).isActive = true
         
+        portfolioButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        portfolioButton.trailingAnchor.constraint(equalTo: marketplaceButton.leadingAnchor).isActive = true
+        portfolioButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        portfolioButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        portfolioButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3333).isActive = true
+        
+        marketplaceButton.leadingAnchor.constraint(equalTo: portfolioButton.trailingAnchor).isActive = true
+        marketplaceButton.trailingAnchor.constraint(equalTo: leaguesButton.leadingAnchor).isActive = true
+        marketplaceButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        marketplaceButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        marketplaceButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3333).isActive = true
+        
+        leaguesButton.leadingAnchor.constraint(equalTo: marketplaceButton.trailingAnchor).isActive = true
+        leaguesButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        leaguesButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        leaguesButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        
+        // Add button touch handlers
+        portfolioButton.addTarget(self, action: #selector(MSTabBarViewController.barButtonPressed(button:)), for: .touchUpInside)
+        marketplaceButton.addTarget(self, action: #selector(MSTabBarViewController.barButtonPressed(button:)), for: .touchUpInside)
+        leaguesButton.addTarget(self, action: #selector(MSTabBarViewController.barButtonPressed(button:)), for: .touchUpInside)
     }
+    
+    @objc func barButtonPressed(button: UIButton) {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 10.5, animations: { [unowned self] in
+            button.setTitleColor(UIColor.black, for: .normal)
+            if button != self.portfolioButton {
+                self.portfolioButton.setTitleColor(UIColor.gray, for: .normal)
+            }
+            if button != self.marketplaceButton {
+                self.marketplaceButton.setTitleColor(UIColor.gray, for: .normal)
+            }
+            if button != self.leaguesButton {
+                self.leaguesButton.setTitleColor(UIColor.gray, for: .normal)
+            }
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    
 }
