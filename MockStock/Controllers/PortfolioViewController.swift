3171dd02830e74b1263ce09bad17a2a42628e5b9
Trey <thecht182@gmail.com>
2019-03-11 12:19:06 -0400

    adds portfolio labels

--- MockStock/Controllers/PortfolioViewController.swift
+++ MockStock/Controllers/PortfolioViewController.swift
@@ -13,40 +13,66 @@ class PortfolioViewController: UIViewController {
     var headerLabel: UILabel = {
         let l = UILabel()
         l.text = "Portfolio"
+        l.font = UIFont(name: "Futura-CondensedExtraBold", size: 48)
+        l.textColor = UIColor.black
+        l.translatesAutoresizingMaskIntoConstraints = false
         return l
     }()
     var networth: UILabel = {
         let l = UILabel()
         l.text = "Net Worth:"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
         return l
     }()
     var networthValue: UILabel = {
         let l = UILabel()
         l.text = "9001"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.textAlignment = .right
         return l
     }()
     var buyingPower: UILabel = {
         let l = UILabel()
         l.text = "Buying Power:"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
         return l
     }()
     var buyingpowerValue: UILabel = {
         let l = UILabel()
         l.text = "9001"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.textAlignment = .right
         return l
     }()
     var portfolio: UILabel = {
         let l = UILabel()
         l.text = "Portfolio Value:"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
         return l
     }()
     var portfolioValue: UILabel = {
         let l = UILabel()
         l.text = "9001"
+        l.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+        l.textColor = .gray
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.textAlignment = .right
         return l
     }()
     var divider: UIView = {
         let v = UIView()
+        v.backgroundColor = .gray
+        v.translatesAutoresizingMaskIntoConstraints = false
         return v
     }()
     
@@ -56,13 +82,61 @@ class PortfolioViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = .white
         
-        // Test label -- Remove it later
-        let labelView = UILabel()
-        labelView.text = "Portfolio View! (Remove this label later)."
-        labelView.textColor = UIColor.black
-        labelView.frame = CGRect(x: 25, y: 100, width: 500, height: 25)
-        view.addSubview(labelView)
-        // End test label
+        // Add header views to the root view
+        view.addSubview(headerLabel)
+        view.addSubview(networth)
+        view.addSubview(networthValue)
+        view.addSubview(buyingPower)
+        view.addSubview(buyingpowerValue)
+        view.addSubview(portfolio)
+        view.addSubview(portfolioValue)
+        view.addSubview(divider)
+        
+        // Add constraints to the header views
+        let leftInset = CGFloat(30.0)
+        let rightInset = CGFloat(-30.0)
+        let topInset = CGFloat(30.0)
+        
+        headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topInset).isActive = true
+        headerLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leftInset).isActive = true
+        headerLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: rightInset).isActive = true
+        headerLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        networth.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 10).isActive = true
+        networth.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: leftInset).isActive = true
+        networth.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+        networth.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        networthValue.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 10).isActive = true
+        networthValue.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+        networthValue.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: rightInset).isActive = true
+        networthValue.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        buyingPower.topAnchor.constraint(equalTo: networth.bottomAnchor, constant: 10).isActive = true
+        buyingPower.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
+        buyingPower.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        buyingPower.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        buyingpowerValue.topAnchor.constraint(equalTo: networth.bottomAnchor, constant: 10).isActive = true
+        buyingpowerValue.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        buyingpowerValue.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: rightInset).isActive = true
+        buyingpowerValue.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        portfolio.topAnchor.constraint(equalTo: buyingPower.bottomAnchor, constant: 10).isActive = true
+        portfolio.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: leftInset).isActive = true
+        portfolio.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        portfolio.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        portfolioValue.topAnchor.constraint(equalTo: buyingPower.bottomAnchor, constant: 10).isActive = true
+        portfolioValue.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        portfolioValue.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: rightInset).isActive = true
+        portfolioValue.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        divider.topAnchor.constraint(equalTo: portfolio.bottomAnchor, constant: 30).isActive = true
+        divider.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        divider.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        divider.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
         
     }
 
