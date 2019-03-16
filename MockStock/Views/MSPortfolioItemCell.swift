975f7ba28456d62fbe8bf539088eb5f96200011f
Trey <thecht182@gmail.com>
2019-03-16 13:26:13 -0400

    adds MSPortfolioItemCell color changing

--- MockStock/Views/MSPortfolioItemCell.swift
+++ MockStock/Views/MSPortfolioItemCell.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class MSPortfolioItemCell: UICollectionViewCell {
     
+    // Background bars
     var colorView1: UIView = {
         let v = UIView()
         v.backgroundColor = UIColor.darkGray
@@ -22,11 +23,21 @@ class MSPortfolioItemCell: UICollectionViewCell {
         return v
     }()
     
+    // Labels
+    var priceLabel: UILabel!
+    var priceValueLabel: UILabel!
+    var quantityLabel: UILabel!
+    var quantityValueLabel: UILabel!
+    var tcbLabel: UILabel!
+    var tcbValueLabel: UILabel!
+    var tickerLabel: UILabel!
+    var valuePercentLabel: UILabel!
+    var valueLabel: UILabel!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
-    
     
     func setupViews() {
         // Configure view properties
@@ -46,29 +57,16 @@ class MSPortfolioItemCell: UICollectionViewCell {
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         
-        //        // Configure view properties
-        //        layer.cornerRadius = self.frame.width / 3
-        //        layer.shadowColor = UIColor.black.cgColor
-        //        layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
-        //        layer.shadowOpacity = 0.4
-        //        layer.shadowRadius = 4.0
-        //
-        //        // Add & Configure subviews
-        //        addSubview(colorView1)
-        //        addSubview(colorView2)
-        //
-        //        colorView2.layer.cornerRadius = layer.cornerRadius
-        //
-        //        colorView2.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
-        //        colorView2.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
-        //        colorView2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
-        //        colorView2.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 0.333).isActive = true
-        
-        
+        // Size background views
         colorView2.frame = CGRect(x: 0, y: frame.height - frame.height * 0.4, width: frame.width, height: frame.height * 0.4)
         colorView2.layer.cornerRadius = colorView2.frame.width / 25
-        
         colorView1.frame = CGRect(x: 0, y: frame.height - frame.height * 0.5, width: frame.width, height: frame.height * 0.2)
+    }
+    
+    func setColors(topView: UIColor, bottomViewColor: UIColor) {
+        self.backgroundColor = topView
+        self.colorView1.backgroundColor = bottomViewColor
+        self.colorView2.backgroundColor = bottomViewColor
     }
     
     required init?(coder aDecoder: NSCoder) {
