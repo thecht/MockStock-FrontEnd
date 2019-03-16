57131083805427e714ec974bc4588acb80974117
Trey <thecht182@gmail.com>
2019-03-16 12:36:49 -0400

    adds portfolio item cell areas

--- MockStock/Controllers/PortfolioViewController.swift
+++ MockStock/Controllers/PortfolioViewController.swift
@@ -82,7 +82,6 @@ class PortfolioViewController: UIViewController {
         v.translatesAutoresizingMaskIntoConstraints = false
         v.alwaysBounceVertical = true
         v.backgroundColor = .white
-        v.register(MSPortfolioItemCell.self, forCellWithReuseIdentifier: "PortfolioItem")
         return v
     }()
     var portfolioItems = [MSPortfolioItem]()
@@ -107,6 +106,7 @@ class PortfolioViewController: UIViewController {
         view.addSubview(collectionView)
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.register(MSPortfolioItemCell.self, forCellWithReuseIdentifier: "PortfolioItem")
         
         // Add constraints to the header views
         let leftInset = CGFloat(30.0)
@@ -169,43 +169,98 @@ class PortfolioViewController: UIViewController {
             layout.sectionInset = UIEdgeInsets(top: 35, left: 0, bottom: 45, right: 0)
             layout.minimumInteritemSpacing = 15
             layout.minimumLineSpacing = 15
-            layout.itemSize = CGSize(width: collectionView.frame.width - 60, height: collectionView.frame.width * 0.225)
+            //layout.itemSize = CGSize(width: collectionView.frame.width - 60, height: collectionView.frame.width * 0.225)
         }
     }
 
 }
 
-extension PortfolioViewController: UICollectionViewDataSource {
+extension PortfolioViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5 //portfolioItems.count
+        return 5 // portfolioItems.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        let modelItem = portfolioItems[indexPath.item]
         let c = collectionView.dequeueReusableCell(withReuseIdentifier: "PortfolioItem", for: indexPath) as! MSPortfolioItemCell
         c.translatesAutoresizingMaskIntoConstraints = false
-        c.backgroundColor = .green
+        c.backgroundColor = .gray
         return c
     }
     
-}
-
-extension PortfolioViewController: UICollectionViewDelegate {
-    
-}
-
-class MSPortfolioItemCell: UICollectionViewCell {
-    
-    override func didMoveToSuperview() {
-        super.didMoveToSuperview()
-        layer.cornerRadius = self.frame.width / 3
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
-        layer.shadowOpacity = 0.4
-        layer.shadowRadius = 4.0
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width - 60, height: collectionView.frame.width * 0.225)
     }
+    
 }
+
+//class MSPortfolioItemCell: UICollectionViewCell {
+//
+//    var colorView1: UIView = {
+//        let v = UIView()
+//        v.backgroundColor = UIColor.darkGray
+//        return v
+//    }()
+//    var colorView2: UIView = {
+//        let v = UIView()
+//        v.backgroundColor = UIColor.darkGray
+//        return v
+//    }()
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setupViews()
+//    }
+//
+//
+//    func setupViews() {
+//        // Configure view properties
+//        layer.cornerRadius = self.frame.width / 25
+//        layer.shadowColor = UIColor.black.cgColor
+//        layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
+//        layer.shadowOpacity = 0.4
+//        layer.shadowRadius = 4.0
+//
+//        // Add & Configure subviews
+//        addSubview(colorView1)
+//        addSubview(colorView2)
+//
+//
+//
+//    }
+//    override func didMoveToSuperview() {
+//        super.didMoveToSuperview()
+//
+////        // Configure view properties
+////        layer.cornerRadius = self.frame.width / 3
+////        layer.shadowColor = UIColor.black.cgColor
+////        layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
+////        layer.shadowOpacity = 0.4
+////        layer.shadowRadius = 4.0
+////
+////        // Add & Configure subviews
+////        addSubview(colorView1)
+////        addSubview(colorView2)
+////
+////        colorView2.layer.cornerRadius = layer.cornerRadius
+////
+////        colorView2.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
+////        colorView2.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+////        colorView2.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+////        colorView2.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 0.333).isActive = true
+//
+//
+//        colorView2.frame = CGRect(x: 0, y: frame.height - frame.height * 0.4, width: frame.width, height: frame.height * 0.4)
+//        colorView2.layer.cornerRadius = colorView2.frame.width / 25
+//
+//        colorView1.frame = CGRect(x: 0, y: frame.height - frame.height * 0.5, width: frame.width, height: frame.height * 0.2)
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
